61dc73bfb56bcef09eab191ac68f629f42af0f57
Santiago Paccini <santiago.paccini.1403@gmail.com>
2025-09-17 16:43:36 -0500

    Se crea la vista 'nosotros', se añade su respectiva informacion

--- backend/gisenails/urls.py
+++ backend/gisenails/urls.py
@@ -26,6 +26,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    path('nosotros/', include('nosotros.urls')),
 ]
 
 if settings.DEBUG:
--- backend/nosotros/urls.py
+++ backend/nosotros/urls.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from django.urls import path
 from nosotros import views
 urlpatterns = [
-    path("nosotros/" , views.nosotros , name="nosotros" ),
+    path("" , views.nosotros , name="nosotros" ),
 ]
