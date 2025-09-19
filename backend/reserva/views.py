f8f801670018abcdf75c6bb42052a93dba927bdf
Santiago Paccini <santiago.paccini.1403@gmail.com>
2025-09-18 20:29:10 -0500

    Se crea la estructura completa del formulario de reserva, asi mismo se organiza lo que se lleva de proyecto con una estructura mejor

--- backend/reserva/views.py
+++ backend/reserva/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+# from .models import HorarioDisponible
+
 # from reserva import views
 # Create your views here.
 def reserva(request):
@@ -6,3 +9,15 @@ def reserva(request):
 
 def reserva2(request):
     return render(request, 'reserva/reserva2.html')
+
+# def horarios_disponibles(request):
+#     fecha = request.GET.get('fecha')
+#     if not fecha:
+#         return JsonResponse({'error': 'Fecha no proporcionada'}, status=400)
+#     # Horarios fijos
+#     horarios_fijos = HorarioDisponible.objects.all().order_by('hora')
+#     # Horas ocupadas en la fecha
+#     ocupadas = Cita.objects.filter(fecha=fecha).values_list('hora', flat=True)
+#     # Horas libres
+#     libres = [h.hora.strftime('%H:%M') for h in horarios_fijos if h.hora not in ocupadas]
+#     return JsonResponse({'horarios': libres})
