33a8cef3e5ee11b612822e3f3d6931d1d88a8eca
Juan Arcila <juanvillamarin2000@gmail.com>
2025-09-24 16:16:28 -0500

    Creada la paginacion de productos y cargadas las imagenes

--- backend/productos/views.py
+++ backend/productos/views.py
@@ -1,10 +1,13 @@
-
 from django.shortcuts import render
 from .models import Producto
+from django.core.paginator import Paginator
 from django.views.decorators.http import require_GET
 
 @require_GET
 def lista_productos(request):
-	productos = Producto.objects.all()
-	return render(request, 'productos/lista_productos.html', {'productos': productos})
+    productos_list = Producto.objects.all()
+    paginator = Paginator(productos_list, 2)  # 2 productos por página
+    page_number = request.GET.get('page')
+    productos = paginator.get_page(page_number)
+    return render(request, 'productos/lista_productos.html', {'productos': productos})
 
