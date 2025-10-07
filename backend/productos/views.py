06fd706ce3dacc53dac3646e0d129ed60bf9ac8b
Juan Arcila <juanvillamarin2000@gmail.com>
2025-10-07 15:06:58 -0500

    Creado el CRUD de productos

--- backend/productos/views.py
+++ backend/productos/views.py
@@ -1,7 +1,70 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Producto
+from .forms import ProductoForm
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.views.decorators.cache import never_cache
 from django.core.paginator import Paginator
 from django.views.decorators.http import require_GET
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+@never_cache
+def producto_list(request):
+    q = request.GET.get('q', '')
+    productos = Producto.objects.all()
+    if q:
+        productos = productos.filter(nombre__icontains=q)
+    paginator = Paginator(productos, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'productos/producto_list.html', {
+        'productos': page_obj,
+        'q': q,
+        'is_paginated': page_obj.has_other_pages(),
+        'page_obj': page_obj,
+        'paginator': paginator,
+    })
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+@never_cache
+def producto_create(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('producto:producto_list')
+    else:
+        form = ProductoForm()
+    return render(request, 'productos/producto_form.html', {'form': form})
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+@never_cache
+def producto_update(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, request.FILES, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('producto:producto_list')
+    else:
+        form = ProductoForm(instance=producto)
+    return render(request, 'productos/producto_form.html', {'form': form, 'producto': producto})
+
+@login_required
+@user_passes_test(lambda u: u.is_staff)
+@never_cache
+def producto_delete(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('producto:producto_list')
+    return render(request, 'productos/producto_confirm_delete.html', {'producto': producto})
+
+def producto_detail(request, pk):
+    producto = get_object_or_404(Producto, pk=pk)
+    return render(request, 'productos/producto_detail.html', {'producto': producto})
 
 @require_GET
 def lista_productos(request):
@@ -10,4 +73,3 @@ def lista_productos(request):
     page_number = request.GET.get('page')
     productos = paginator.get_page(page_number)
     return render(request, 'productos/lista_productos.html', {'productos': productos})
-
