508e9448a653237403cb52fa708f2b8c25808cfd
Juan Arcila <juanvillamarin2000@gmail.com>
2025-10-08 16:49:00 -0500

    crado la ventana modal para actualizar datos del admin y creada campana de notificaciones

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -6,6 +6,7 @@ app_name = 'login'
 urlpatterns = [
     path("", views.login_view, name='login'),
     path("admin-panel/", views.admin_panel, name='admin_panel'), 
-    path("dashboard/", views.dashboard, name='dashboard'),  # <-- Nueva ruta
+    path("dashboard/", views.dashboard, name='dashboard'),
     path("logout/", views.logout_view, name='logout'),
+    path("update-user/", views.update_user, name='update_user'),  # <-- Nueva ruta
 ]
