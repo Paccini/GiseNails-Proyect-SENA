ba1d4386e6cdf879349220e72ebe54edced08bfe
Juan Arcila <juanvillamarin2000@gmail.com>
2025-11-28 12:48:02 -0500

    implementada la opcion de restablecer contraseña

--- backend/login/urls.py
+++ backend/login/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
     path("logout/", views.logout_view, name='logout'),
     path('registro/', views.registro_cliente, name='registro'),
     path("update-user/", views.update_user, name='update_user'),  # <-- Nueva ruta
+    path("password-reset/", views.password_reset_view, name='password_reset'),
+    path("password-reset-confirm/<str:token>/", views.password_reset_confirm_view, name='password_reset_confirm'),
 ]
